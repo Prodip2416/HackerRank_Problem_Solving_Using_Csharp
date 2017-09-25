3cfb122ddee2323e1372a59d4175b23be7aaef9a
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-09-26 02:25:19 +0600

    C#

---
+++ Algorithms/Strings/CamelCase/CamelCase/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Console.Write(s.Count(char.IsUpper)+1);
+        }
+    }
+}
