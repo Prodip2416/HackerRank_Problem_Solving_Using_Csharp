b4b13fdccb8bbe9f8f8c28de3eac080817a4043f
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-09-27 13:40:45 +0600

    C#

---
+++ Algorithms/Sorting/BigSorting/BigSorting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BigSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] unsorted = new string[n];
+            for (int unsorted_i = 0; unsorted_i < n; unsorted_i++)
+            {
+                unsorted[unsorted_i] = Console.ReadLine();
+            }
+            Array.Sort(unsorted, (string a, string b) => {
+                if (a.Length == b.Length)
+                    return string.Compare(a, b, StringComparison.Ordinal);
+                return a.Length - b.Length;
+            });
+            Console.WriteLine(string.Join("\n", unsorted));
+        }
+    }
+}
