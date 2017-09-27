07f02da11e1ce5d469f9cc84dd54343da66ec8ad
Prodip Sarker <prodip_sarker@diu.edu.bd>
2017-09-27 13:42:01 +0600

    C#

---
+++ Algorithms/Strings/FunnyString/FunnyString/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static string funnyString(string s)
+    {
+        for (int i = 0; i < s.Length / 2; i++)
+        {
+            int l = (s.Length - 1) - i;
+            int a = (int)s[i];
+            int b = (int)s[i + 1];
+            int c = (int)s[l];
+            int d = (int)s[l - 1];
+
+            if (Math.Abs(a - b) != Math.Abs(c - d))
+                return "Not Funny";
+        }
+        return "Funny";
+    }
+
+    static void Main(String[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < q; a0++)
+        {
+            string s = Console.ReadLine();
+            string result = funnyString(s);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Algorithms/Strings/MarsExploration/MarsExploration/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MarsExploration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i % 3 == 0 || i % 3 == 2)
+                {
+                    if (s[i] != 'S') count++;
+                }
+                else
+                {
+                    if (s[i] != 'O') count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
