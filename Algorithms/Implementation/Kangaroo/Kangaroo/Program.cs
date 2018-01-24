dad572ef37c6e65bf497d0e6b0a668241ed41fce
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-01-24 20:49:50 +0600

    C#

---
+++ Algorithms/Implementation/Kangaroo/Kangaroo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Program
+{
+    static string kangaroo(int x1, int v1, int x2, int v2)
+    {
+        int x = x1 - x2;
+        int v = v2 - v1;
+        if (v != 0)
+        {
+            if (x % v == 0 && x / v > 0)
+            {
+                return "YES";
+            }
+            else { return "NO"; }
+
+        }
+        else { return "NO"; }
+    }
+
+    static void Main(String[] args)
+    {
+        string[] tokens_x1 = Console.ReadLine().Split(' ');
+        int x1 = Convert.ToInt32(tokens_x1[0]);
+        int v1 = Convert.ToInt32(tokens_x1[1]);
+        int x2 = Convert.ToInt32(tokens_x1[2]);
+        int v2 = Convert.ToInt32(tokens_x1[3]);
+        string result = kangaroo(x1, v1, x2, v2);
+        Console.WriteLine(result);
+    }
+}
