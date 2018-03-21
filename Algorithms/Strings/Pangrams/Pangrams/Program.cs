068a5c8d6daacd3a10f66d535407a484774ad6ea
Prodip Sarker <prodip_sarker@diu.edu.bd>
2018-03-21 15:28:32 +0600

    C#

---
+++ Algorithms/Strings/Pangrams/Pangrams/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Pangrams
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine().ToLower().Replace(" ", "");
+            int[] arr = new int[26];
+            for (int i = 0; (i < s.Length) && (s.Length >= 26); arr[Convert.ToInt16(s[i]) - 97]++, i++) ;
+            Console.WriteLine(arr.Min() > 0 ? "pangram" : "not pangram");
+        }
+    }
+}
---
+++ Algorithms/Strings/Strong Password/Strong Password/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Strong_Password
+{
+    class Program
+    {
+
+        public static int MinimumNumber(string password)
+        {
+            var match = password.IndexOfAny(new char[] { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+' }) != -1;
+            int count = 0;
+            if (password.Any(char.IsLower) && password.Any(char.IsUpper) && password.Any(char.IsDigit) && password.Length > 6 && match == true)
+            {
+                count = 0;
+            }
+            else
+            {
+                if (String.IsNullOrEmpty(password))
+                {
+                    count = 6;
+                }
+                else
+                {
+                    if (!password.Any(char.IsLower))
+                    {
+                        count++;
+                    }
+                    if (!password.Any(char.IsUpper))
+                    {
+                        count++;
+                    }
+                    if (!password.Any(char.IsDigit))
+                    {
+                        count++;
+                    }
+                    if (match == false)
+                    {
+                        count++;
+                    }
+
+                }
+                if (password.Length < 6)
+                {
+                    int wcount = 6 - password.Length;
+                    count = wcount > count ? wcount : count;
+                }
+            }
+            return count;
+        }
+
+        public static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string password = Console.ReadLine();
+            int answer = MinimumNumber(password);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+        }
+
+    }
+}
